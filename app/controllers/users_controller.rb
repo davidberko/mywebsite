d7814695ba2e32db96bceec7ff91af6ed2c3f0f7
David M. Berko <davidberko19@gmail.com>
2013-05-08 19:16:36 -0500

    updated styling of landing page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
+  http_basic_authenticate_with name: "David Berko", password: "db101191"
+
   def new
+    @user = User.new(params[:user])
   end
-end
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to "index"
+    else
+      render "new"
+    end
+  end
+
+  end
+  def index
+    @users = User.all
+  end
+
+
+
